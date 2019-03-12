88a20b07174a62cef0300b6bc83e8201a292bc86
hxsf <i@hxsf.me>
2019-03-12 10:22:10 +0800

    change package name

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,33 +1,33 @@
-import { Channel } from './index'
+import { Channel } from '.'
 
-function dosthAsync(ms: number): Promise<void> {
+function do_something_async(ms: number): Promise<void> {
     return new Promise<void>((resolve, reject) => {
         setTimeout(() => {
             resolve()
         }, ms)
     })
 }
+
 const chan = new Channel<string>(5)
+const uptime = Date.now()
 const num = 20
 async function pub() {
     for (let i = 0; i< num; i++) {
         const label = i.toString().padStart(3, '0')
         await chan.add(label)
-        await dosthAsync(Math.floor(Math.random() * 40 + 10))
-        console.log(`[add] [${label}] to chan.`)
-        console.log(`[total] ${chan.size()}/${chan.max_size}.`)
+        await do_something_async(50)
+        console.log(`[${Date.now() - uptime}] [add] [${label}] to chan.`)
+        console.log(`[${Date.now() - uptime}] [total] ${chan.size()}/${chan.max_size}.`)
     }
 }
 async function sub() {
     for (let i = 0; i< num; i++) {
         const label = await chan.get()
-        await dosthAsync(Math.floor(Math.random() * 900 + 100))
-        console.log(`[get] [${label}] to chan.`)
-        console.log(`[total] ${chan.size()}/${chan.max_size}.`)
+        await do_something_async(500)
+        console.log(`[${Date.now() - uptime}] [get] [${label}] to chan.`)
+        console.log(`[${Date.now() - uptime}] [total] ${chan.size()}/${chan.max_size}.`)
     }
 }
 
 pub()
-setTimeout(() => {
-    sub()
-}, 3000)
+setTimeout(sub, 2000)
